c6a55868d110f33eb27cc25b2812341bc7b4c548
Michael de Silva <michael@mwdesilva.com>
2016-10-03 12:55:48 +0530

    Run fresh CMS install

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,7 +27,11 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
-  config.action_mailer.perform_caching = false
+  if config.respond_to?(:action_mailer)
+  if config.respond_to?(:action_mailer)
+      config.action_mailer.perform_caching = false
+  end
+  end
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
